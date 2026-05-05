01baa8aa58e8383980cc5cc12ce6e13e56eebc88
Pentagon_Invent <pentagonmediagroupllc@gmail.com>
2026-05-05 03:03:26 -0400

    Delete checkflow-main directory

--- checkflow-main/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: { domains: ['uxxilsmxwbqefjsfszom.supabase.co'] },
-}
-module.exports = nextConfig
